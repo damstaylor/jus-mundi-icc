a59503c4ca5b832baa191975988f54c58299f8e6
damstaylor <damiensabo@live.fr>
2023-12-26 23:00:20 +0100

    Move button styles to component & add hover effect

--- components/button.vue
+++ components/button.vue
@@ -15,9 +15,36 @@ export default {
 </script>
 <style scoped>
 .button {
+  font-family: Lato;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  border: unset;
   display: flex;
   align-items: center;
   justify-content: center;
+  min-width: 32px;
+  padding: 20px 32px;
+  gap: 8px;
+  border-radius: 4px;
+  box-shadow: 0px 8px 32px 0px rgba(37, 79, 109, 0.24);
+
+  &:hover {
+    opacity: .75;
+  }
+
+  &:not(:disabled) {
+    cursor: pointer;
+    
+    & img {
+      width: 12px;
+      height: 12px;
+      padding: 6px;
+    };
+  }
 
   &.primary {
     color: var(--white);

ae8e53973fed7b4fca7760c703ea3403cc91176c
damstaylor <damiensabo@live.fr>
2023-12-27 22:08:55 +0100

    Reindent main-content.vue & create --faded-primary-color variable

--- components/main-content.vue
+++ components/main-content.vue
@@ -7,8 +7,9 @@
     <div class="two-columns">
       <div class="left">
         <CircularIcon :src="imgPath('logo_icc.svg')" alt="ICC logo" size="483px" class="icc-icon" />
-        <CircularIcon :src="imgPath('logo_jm.svg')" alt="JM logo" size="187px" bg-color="rgba(255, 255, 255, 0.75)" class="jm-icon" />
-        <img class="animation" :src="imgPath('case_bg.gif')" alt="Animation" />
+        <CircularIcon :src="imgPath('logo_jm.svg')" alt="JM logo" size="187px" bg-color="var(--faded-primary-color)"
+          class="jm-icon" />
+        <img class="gif-animation" :src="imgPath('case_bg.gif')" alt="Animation" />
       </div>
       <div class="right">
         <div class="text">
@@ -58,7 +59,8 @@ export default {
   & footer {
     display: flex;
     height: 120px;
-    margin-top: -55px; /* To be improved */
+    margin-top: -55px;
+    /* To be improved */
   }
 
   .line-container {
@@ -81,50 +83,50 @@ export default {
     display: flex;
     justify-content: space-between;
     gap: 48px;
-    
-      .left {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        position: relative;
-    
-        .animation {
-          width: 478px;
-          height: 496px;
-          margin-top: auto;
-        }
-    
-        .circular-icon {
-          position: absolute;
-        }
-    
-        .icc-icon {
-          bottom: 214px;
-          left: 30px;
-        }
-    
-        .jm-icon {
-          bottom: 64px;
-          left: 84px;
-        }
+
+    .left {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      position: relative;
+
+      .gif-animation {
+        width: 478px;
+        height: 496px;
+        margin-top: auto;
       }
-    
-      .right {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        margin-right: 57px;
-        padding-bottom: 24px;
-    
-        .text {
-          width: 582px;
-          margin-bottom: 93px;
-        }
-    
-        .button {
-          z-index: 1;
-        }
+
+      .circular-icon {
+        position: absolute;
       }
+
+      .icc-icon {
+        bottom: 214px;
+        left: 30px;
+      }
+
+      .jm-icon {
+        bottom: 64px;
+        left: 84px;
+      }
+    }
+
+    .right {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin-right: 57px;
+      padding-bottom: 24px;
+
+      .text {
+        width: 582px;
+        margin-bottom: 93px;
+      }
+
+      .button {
+        z-index: 1;
+      }
+    }
   }
 }
 </style>

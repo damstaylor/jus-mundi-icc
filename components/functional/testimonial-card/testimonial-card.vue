1094af32e64558657a784c2df03c24876dee93ae
damstaylor <damiensabo@live.fr>
2024-04-24 23:28:45 +0200

    Use absolute paths for SVG images in testimonial-card.vue

--- components/functional/testimonial-card/testimonial-card.vue
+++ components/functional/testimonial-card/testimonial-card.vue
@@ -3,13 +3,13 @@
     <figure>
       <blockquote>
         <div class="image-wrapper opening-quote">
-          <img class="image" alt="Quote sign 1" src="assets/images/quote_2.svg" />
+          <img class="image" alt="Quote sign 1" src="public/assets/images/quote_2.svg" />
         </div>
         <p class="quote">Jus Mundi is an indispensable tool to meet the needs of our clients! It is cost-effective and
           gives us access
           to key information to build the winning strategy.</p>
         <div class="image-wrapper closing-quote">
-          <img class="image" alt="Quote sign 2" src="assets/images/quote_1.svg" />
+          <img class="image" alt="Quote sign 2" src="public/assets/images/quote_1.svg" />
         </div>
       </blockquote>
       <figcaption>
